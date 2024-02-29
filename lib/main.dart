d891bbed9dd4f794aad14e2325df33816c1be698
Aaron Burciaga <146780951+BurciagaAA128@users.noreply.github.com>
2024-02-29 11:58:39 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,86 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const AppMiTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTabBar extends StatelessWidget {
+  const AppMiTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar Aaron Burciaga",
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MiPaginaInicial(),
     );
   }
-}
+} //Fin AppMiTabBar
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+//Stateful
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} //MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Tabbar Aaron Burciaga"),
+          centerTitle: true,
+          bottom: TabBar(tabs: [
+            Tab(
+              text: "Platillos",
+              icon: Icon(Icons.lunch_dining),
+            ),
+            Tab(
+              text: "Bebidas",
+              icon: Icon(Icons.no_drinks),
+            ),
+            Tab(
+              text: "Gerente",
+              icon: Icon(Icons.badge),
+            ),
+            Tab(
+              text: "Clientes",
+              icon: Icon(Icons.people),
+            ),
+          ] //Fin de tabs
+              ), //Fin bottom TabBar
         ),
-      ),
-    );
-  }
-}
+        body: TabBarView(children: const <Widget>[
+          Center(
+            child: Text(
+              "Hamburguesa",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Tequila",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Aarón",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Valeria",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+        ] //Jardin de niños
+            ), //Fin de tabbarview
+      ), //Fin de scaffold
+    ); //Fin DefaultTabController
+  } // Fin widget
+} //_MiPaginaInicialState
